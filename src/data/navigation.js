10c2b6789667e5e7cdb6fcf07cced5fb163c109f
sammy.bruce@Hotmail.com <sammy.bruce@hotmail.com>
2025-02-18 20:49:57 -0600

    first commit

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -3,5 +3,6 @@ export const navItems = [
     { path: '/', label: 'Home' },
     { path: '/about', label: 'About Us' },
     { path: '/contact', label: 'Contact' },
-    { path: '/events', label: 'Events'}
+    { path: '/events', label: 'Events'},
+    { path: '/donate', label: 'Donate'}
 ];
