d327d86800b049e36ffec73c2dcbfb92312e29aa
sammy.bruce@Hotmail.com <sammy.bruce@hotmail.com>
2025-02-18 18:02:31 -0600

    first try

---
+++ src/data/navigation.js
@@ -0,0 +1,7 @@
+// src/data/navigation.js
+export const navItems = [
+    { path: '/', label: 'Home' },
+    { path: '/about', label: 'About Us' },
+    { path: '/contact', label: 'Contact' },
+    { path: '/events', label: 'Events'}
+];
